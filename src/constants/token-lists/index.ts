c6a3d3c3ef13278f39c0ab636c7d9f7dbad44a45
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2021-07-06 18:32:51 +0200

    feat: initial basic integration of manifold

--- src/constants/token-lists/index.ts
+++ src/constants/token-lists/index.ts
@@ -17,12 +17,17 @@ const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
+<<<<<<< HEAD
 const KLEROS_LIST = 't2crtokens.eth'
 export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+=======
+const QUICK_SWAP = 'https://unpkg.com/quickswap-default-token-list@1.0.39/build/quickswap-default.tokenlist.json'
+const MANIFOLD_LIST = 'https://raw.githubusercontent.com/manifoldfinance/token-list/main/manifold.tokenlist.json'
+>>>>>>> 6b1f8fc0 (feat: initial basic integration of manifold)
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
